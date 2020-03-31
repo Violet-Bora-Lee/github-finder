1d4a358a0d94d1e4fdcca9aac5fe067461750f7a
Violet Bora Lee <learnerbr@gmail.com>
2020-03-31 17:52:04 +0900

    test axios using GitHub API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navbar from './Components/layout/Navbar';
 import Users from './Components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://api.github.com/users').then(res => console.log(res.data))
+  }
 
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar/>
         <div className="container">
-          <Users />
+          <Users/>
         </div>
       </div>
     );
