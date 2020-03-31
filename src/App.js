8e1b6e739718f4c0a242b42936418e4751a9ca99
Violet Bora Lee <learnerbr@gmail.com>
2020-03-31 13:55:49 +0900

    Implement UserItem Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Navbar from './Components/layout/Navbar'
+import Navbar from './Components/layout/Navbar';
+import UserItem from './Components/users/UserItem';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
     );
   }
---
+++ src/Components/users/UserItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+
+    state = {
+        id: 'id',
+        login: 'Violet-Bora-Lee',
+        avatar_url: 'https://avatars3.githubusercontent.com/u/6451384?v=4',
+        html_url: 'https://github.com/Violet-Bora-Lee'
+    };
+
+    render() {
+        const { login, avatar_url, html_url } = this.state;
+
+        return (
+            <div className='card text-center'>
+                <img 
+                    src={avatar_url} 
+                    alt='' 
+                    className='round-img' 
+                    style={{width: '60px'}}
+                />
+                <h3>{login}</h3>
+
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                        More
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserItem
