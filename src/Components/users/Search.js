4fc6c5d9dcb21c28c2dec97e391a6c6e891cea3b
Violet Bora Lee <learnerbr@gmail.com>
2020-04-02 20:27:08 +0900

    Implement clearing users from the state

--- src/Components/users/Search.js
+++ src/Components/users/Search.js
@@ -8,8 +8,10 @@ class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
-  }
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
 
   onChange = (e) => {
     this.setState({
@@ -36,6 +38,15 @@ class Search extends Component {
           />
           <input type="submit" value="Search" className="btn btn-dark btn-block"/>
         </form>
+        {this.props.showClear &&
+        <button
+          className="btn btn-light btn-block"
+          onClick={this.props.clearUsers}>
+          Clear
+        </button>
+        }
+
+
       </div>
     );
   }
